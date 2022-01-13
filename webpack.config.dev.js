a5df8e45bb1a4855ae075950593a75beaee6cb70
bstrubeXpanxion <bstrube@xpanxion.com>
2022-01-12 22:57:05 -0500

    updated webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,5 +13,32 @@ module.exports = {
         path: path.resolve(__dirname, "build"),
         publicPath: '/',
         filename: 'bundle.js',
+    },
+    devServer: {
+        stats: 'minimal',
+        overlay: true,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: {"Access-Control-Allow-Origin": "*"},
+        https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            favicon: "src/favicon.ico"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
     }
 }
