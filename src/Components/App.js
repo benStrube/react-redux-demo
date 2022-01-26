14cf608adcd0c3be0983001c7c0852cf6615036e
bstrubeXpanxion <bstrube@xpanxion.com>
2022-01-26 16:13:55 -0500

    added page not found component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './Home/HomePage';
 import AboutPage from './About/AboutPage';
 import Header from "./Common/Header";
+import PageNotFound from './PageNotFound';
 
 function App() {
     return (
-        <div className='container-fluid'>
-            <Header />
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/about" component={AboutPage}/>
-        </div>
-    )
+      <div className="container-fluid">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    );
 }
 
 export default App;
