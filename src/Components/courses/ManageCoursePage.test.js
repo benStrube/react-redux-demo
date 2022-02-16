3ac51eca0573dc548aa5533fa0b209d5c5600109
bstrubeXpanxion <bstrube@xpanxion.com>
2022-02-16 11:18:39 -0500

    testing connected react components

---
+++ src/Components/courses/ManageCoursePage.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { mount } from "enzyme";
+import { authors, newCourse, courses } from "../../../tools/mockData";
+import { ManageCoursePage } from "./ManageCoursePage";
+
+function render(args) {
+  const defaultProps = {
+    authors,
+    courses,
+    //passed from react router in real app, so just stubbing in for test.
+    //could also choose to use MemoryRouter as shown in header.test.js
+    //or even wrap with react router, depending on whether I
+    //need to test react router related behavior
+    history: {},
+    saveCourse: jest.fn(),
+    loadAuthors: jest.fn(),
+    loadCourses: jest.fn(),
+    course: newCourse,
+    match: {},
+  };
+
+  const props = { ...defaultProps, ...args };
+
+  return mount(<ManageCoursePage {...props} />);
+}
+
+it("sets error when attempting to save an empty title field", () => {
+  const wrapper = render();
+  wrapper.find("form").simulate("submit");
+  const error = wrapper.find(".alert").first();
+  expect(error.text()).toBe("Title is required.");
+});
