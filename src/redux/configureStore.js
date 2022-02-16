8630618fe9e8645ade9c8409a2767db7999aea6f
bstrubeXpanxion <bstrube@xpanxion.com>
2022-02-16 14:33:48 -0500

    production test run

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 //use commonJS require below so we can dynamically import during build-time
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./confitureStore.prod");
+  module.exports = require("./configureStore.prod");
 } else {
   module.exports = require("./configureStore.dev");
 }
