086118318109ca5ee7663d296a95bf290768c395
bstrubeXpanxion <bstrube@xpanxion.com>
2022-02-16 14:12:16 -0500

    configured store for production

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose; //add support for redux dev tools
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+//use commonJS require below so we can dynamically import during build-time
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./confitureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }
